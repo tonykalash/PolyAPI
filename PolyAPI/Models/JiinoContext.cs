051b9ce2e861fb060157a2e4be043de3f0427fde
tonykalash <antonym80@gmail.com>
2023-04-03 14:20:49 +0300

    Initial commit

---
+++ PolyAPI/Models/JiinoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PolyAPI.Models
+{
+    public partial class JiinoContext : DbContext
+    {
+        public JiinoContext() 
+        {
+        }
+
+        public JiinoContext(DbContextOptions<JiinoContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseMySQL("Server=localhost; Database=burnfeniks_mgok11; User ID=046351469_mgok11; Password = huI1lM5MP2; Trusted_Connection=False; Integrated Security=False; TrustServerCertificate=True;");
+        
+    }
+}
